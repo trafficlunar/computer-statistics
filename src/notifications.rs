65f389c81ef29c1316ea48fc49668146ffbc3f32
trafficlunar <hello@trafficlunar.net>
2025-07-22 14:23:10 +0100

    feat: resort to printing error if sending notification fails

--- src/notifications.rs
+++ src/notifications.rs
@@ -3,11 +3,13 @@ use std::time::Duration;
 use notify_rust::Notification;
 
 pub fn send_error_notification(body: &str) {
-    Notification::new()
+    if let Err(e) = Notification::new()
         .summary("Computer Statistics Error")
         .body(body)
         .icon("dialog-error")
         .timeout(Duration::from_millis(60000))
         .show()
-        .unwrap();
+    {
+        eprintln!("Failed to send error notification: {}", e);
+    }
 }
